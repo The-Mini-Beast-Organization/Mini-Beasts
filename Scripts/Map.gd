d0f22d4699d31c52f57e29dd2d1cf16ea3b13273
Clay Ankeny <64806986+Clankhay@users.noreply.github.com>
2021-10-24 16:43:42 -0600

    Added a few vars
    Just getting back into it

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,13 +1,6 @@
 extends Node2D
 
-#List<List<Tile>> tileGrid
-#well, time to figure out 2d arrays I guess
+# make 2d array 'tileGrid' here
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func GetTile(location : Vector2):
+	pass #return tile from tileGrid here
---
+++ Scripts/Tile.gd
@@ -0,0 +1,4 @@
+extends Position2D
+
+var element : String
+var mapPosition : Vector2
